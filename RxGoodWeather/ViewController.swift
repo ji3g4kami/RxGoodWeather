87bff2049513252349dcecbb35e91057f344a4ac
ji3g4kami <ji3g4kami@gmail.com>
2020-07-12 22:46:53 +0100

    [add] weather interface

---
+++ RxGoodWeather/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RxGoodWeather
+//
+//  Created by 登秝吳 on 12/07/2020.
+//  Copyright © 2020 登秝吳. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+  @IBOutlet weak var cityNameTextField: UITextField!
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var humidityLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
