5db85983eb282c2bbe90379ac08d4e01fbec09ed
ji3g4kami <ji3g4kami@gmail.com>
2020-07-13 11:07:34 +0100

    [modify] use binding observables

--- RxGoodWeather/ViewController.swift
+++ RxGoodWeather/ViewController.swift
@@ -51,13 +51,17 @@ final class ViewController: UIViewController {
       return
     }
     let resource = Resource<WeatherResult>(url: url)
-    URLRequest.load(resource: resource)
+    
+    let search = URLRequest.load(resource: resource)
       .observeOn(MainScheduler.instance)
       .catchErrorJustReturn(WeatherResult.empty)
-      .subscribe(onNext: { result in
-        let weather = result.main
-        self.displayWeather(weather)
-      })
+    
+    search.map { "\($0.main.temp) ℉"}
+      .bind(to: self.temperatureLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    search.map { "\($0.main.humidity) %" }
+      .bind(to: self.humidityLabel.rx.text)
       .disposed(by: disposeBag)
   }
   
